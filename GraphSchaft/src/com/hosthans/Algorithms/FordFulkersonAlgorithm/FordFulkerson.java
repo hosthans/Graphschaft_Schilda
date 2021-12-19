0c32b9e391a23e8dcb1065f5173deabcea22c7af
hanwe <hanweb6@gmail.com>
2021-12-19 12:26:35 +0100

    FordFulkerson implementation (added class and Constructor - started Alg with adding the method to initialize every flow with null)

---
+++ GraphSchaft/src/com/hosthans/Algorithms/FordFulkersonAlgorithm/FordFulkerson.java
@@ -0,0 +1,35 @@
+package com.hosthans.Algorithms.FordFulkersonAlgorithm;
+
+import com.hosthans.Graph.Graph;
+import com.hosthans.Graph.Node;
+import com.hosthans.Graph.Vertex;
+
+public class FordFulkerson {
+
+    Graph graph;
+    Vertex Quelle;
+    Vertex Senke;
+
+    public FordFulkerson(String Quelle, String Senke, Graph graph){
+        this.graph = graph;
+        this.Quelle = graph.Knoten.get(Quelle);
+        this.Senke = graph.Knoten.get(Senke);
+        doAlgorithm();
+    }
+
+    public void doAlgorithm(){
+        for (Vertex v : graph.graph.keySet()){
+            for (Node n : graph.graph.get(v)){
+                n.getE().weight = 0;
+            }
+        }
+        //test if it works (every flow set to null)
+        /*
+        for (Vertex v : graph.graph.keySet()){
+            for (Node n : graph.graph.get(v)){
+                System.out.println(n.getE().weight);
+            }
+        }*/
+    }
+
+}
--- GraphSchaft/src/com/hosthans/Graph/test.java
+++ GraphSchaft/src/com/hosthans/Graph/test.java
@@ -1,19 +1,20 @@
 package com.hosthans.Graph;
 
+import com.hosthans.Algorithms.FordFulkersonAlgorithm.FordFulkerson;
 import com.hosthans.Algorithms.PrimAlgorithm.PrimAlgorithm;
 
 import java.io.IOException;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        testGraphwoutInput graph = new testGraphwoutInput();
-        Vertex v1 = new Vertex("a");
-        Vertex v2 = new Vertex("b");
-        System.out.println(graph.printGraph());
 
         Graph g = new Graph(false, false, true);
         System.out.println(g.printGraph());
+        /*
+        PrimAlgorithm prim = new PrimAlgorithm(g);
+        prim.doalgotithm();*/
 
+        FordFulkerson ford = new FordFulkerson("a", "c", g);
 
     }
 }
