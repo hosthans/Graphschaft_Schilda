ced5ef945281b33570c36bb26e67447094fa20fb
hanwe <hanweb6@gmail.com>
2021-12-28 17:19:32 +0100

    fordfulkerson

--- GraphSchaft/src/com/hosthans/Algorithms/Wasserleitung/MaxFlow.java
+++ GraphSchaft/src/com/hosthans/Algorithms/Wasserleitung/MaxFlow.java
@@ -3,26 +3,35 @@ package com.hosthans.Algorithms.Wasserleitung;
 import com.hosthans.Graph.*;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MaxFlow {
 
-    Graph graph;
+    Graph_NOINPUT graph;
     Vertex Quelle;
     Vertex Senke;
-    Graph residual;
+    Graph_NOINPUT residual;
 
     int maxFlow = 0;
 
-    public MaxFlow(String Quelle, String Senke, Graph graph) throws IOException {
+    public MaxFlow(String Quelle, String Senke, Graph_NOINPUT graph) throws IOException {
         this.graph = graph;
         this.Quelle = graph.Knoten.get(Quelle);
         this.Senke = graph.Knoten.get(Senke);
         this.residual = graph;
-        try {
-            initialize();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        initialize();
+        ford();
+    }
+
+    public MaxFlow(Vertex Quelle, Vertex Senke, Graph_NOINPUT graph) throws IOException {
+        this.graph = graph;
+        this.Quelle = Quelle;
+        this.Senke = Senke;
+        this.residual = graph;
+        initialize();
+        ford();
     }
 
     //erst initialisieren (hier mit residualgraph)
@@ -30,7 +39,7 @@ public class MaxFlow {
         for (Vertex v : graph.graph.keySet()){
             for (Node n : graph.graph.get(v)){
                 if (n.getE().weight != 0){
-                    residual.addReverseEdge(n.getE().dest, v);
+                    this.residual.addReverseEdge(n.getE().dest, v);
                 }
             }
         }
@@ -39,40 +48,170 @@ public class MaxFlow {
 
     }
 
-    public void doAlgorithm(Graph graph, Vertex QUelle, Vertex Senke) throws IOException {
-
-    }
-
-
-    /*public boolean Breitensuche (Vertex start, Vertex ende){
+    //nachdem PFad existiert --> erstellen
+    public LinkedList<Vertex> PfadErstellenBreitensuche(Vertex Quelle, Vertex Senke){
+        HashSet<Vertex> visited = new HashSet<>();
         LinkedList<Vertex> queue = new LinkedList<>();
-        queue.push(start);
-        ArrayList<Vertex> list2 = new ArrayList<>();
-        list2.add(start);
-        start.setVisited(true);
-        while(!queue.isEmpty()){
-            Vertex temp = queue.pop();
-            if(temp==ende){
-                for (int i = 0; i<list2.size(); i++){
-                    System.out.println(list2.get(i).getLabel());
-                }
-                return true;
-            }
-            List<Node> list = graph.getNeighbors(temp);
-            for(int i=0;i<list.size();i++){
-                if(!list.get(i).dest.isVisited()){
-                    queue.push(list.get(i).dest);
-                    list2.add(list.get(i).dest);
+        LinkedList<NodeWParent> traversed = new LinkedList<>();
+
+        visited.add(Quelle);
+        queue.addLast(Quelle);
+        traversed.add(new NodeWParent(Quelle));
+
+        while (!queue.isEmpty()){
+            Vertex current = queue.poll();
+
+            for (Node n : residual.getNeighbors(current)){
+                if ((!visited.contains(n.dest)) && (n.getE().getWeight() > 0)){
+                    if (n.getDest().equals(Senke)){
+                        traversed.add(new NodeWParent(n.getDest()));
+                        traversed.getLast().setParent(current);
+                        queue.clear();
+                        break;
+                    }
+                    queue.addLast(n.getDest());
+                    visited.add(n.getDest());
+                    traversed.add(new NodeWParent(n.getDest()));
+                    traversed.getLast().setParent(current);
                 }
             }
         }
 
+        return pfaduebergabe(traversed, Quelle, Senke);
+    }
+
+    public LinkedList<Vertex> pfaduebergabe(LinkedList<NodeWParent> currentList, Vertex Quelle, Vertex Senke){
+        LinkedList<NodeWParent> list = new LinkedList<>();
+        NodeWParent helper = null;
+        for (NodeWParent node : currentList){
+            if (node.getVertex().equals(Senke)){
+                list.add(node);
+            } else if (node.getVertex().equals(Quelle)){
+                helper = node;
+            }
+        }
+
+        while (!list.getLast().getParent().equals(Quelle)){
+            for (NodeWParent node : currentList){
+                if (node.getParent() != null){
+                    if (list.getLast().getParent().equals(node.getVertex())){
+                        list.add(node);
+                    }
+                }
+            }
+
+        }
+        list.add(helper);
+
+        LinkedList<Vertex> listRueckgabe = new LinkedList<>();
+        for (NodeWParent node : list){
+            listRueckgabe.addFirst(node.getVertex());
+        }
+        return listRueckgabe;
+    }
+
+    public boolean PfadExistent(Vertex Quelle, Vertex Senke){
+        HashSet<Vertex> visited = new HashSet<>();
+        LinkedList<Vertex> queue = new LinkedList<>();
+        LinkedList<NodeWParent> traversed = new LinkedList<>();
+
+        visited.add(Quelle);
+        queue.addLast(Quelle);
+        traversed.add(new NodeWParent(Quelle));
+
+        while (!queue.isEmpty()){
+            Vertex current = queue.poll();
+
+            for (Node n : residual.getNeighbors(current)){
+                if ((!visited.contains(n.dest)) && (n.getE().getWeight() > 0)){
+                    //wenn Knoten gleich Endknoten
+                    if (n.dest.equals(Senke)){
+                        traversed.add(new NodeWParent(n.dest));
+                        traversed.getLast().setParent(current);
+                        return true;
+                    }
+                    //Knoten == gesehen --> wird hinzugefügt um diesen in der Liste vorzufinden
+                    queue.addLast(n.getDest());
+                    visited.add(n.getDest());
+                    traversed.add(new NodeWParent(n.getDest()));
+                    traversed.getLast().setParent(current);
+                }
+            }
+        }
         return false;
-    }*/
+    }
+
+    public void ford() throws IOException {
+
+        while (PfadExistent(this.Quelle, this.Senke)){
+            int flow = Integer.MAX_VALUE;
+            LinkedList<Vertex> pfad = PfadErstellenBreitensuche(this.Quelle, this.Senke);
+            int counter = 0;
+
+            for (Vertex v : pfad) {
+                LinkedList<Node> nodes = (LinkedList<Node>) residual.getNeighbors(v);
+                // wir gehen alle knoten des paths bis auf den endknoten ab und speichern seine nachbarn
+                // dann müssen wir den destination-Vertex vom aktuellen knoten vom path in der nodes liste finden
+                // dann weisen wir restcapacity die minimale kantengewichtung zu
+                //System.out.println(v + " - " + nodes);
+                Vertex destVertex = pfad.get((pfad.indexOf(v)) + 1);
+                for (Node n : nodes) {
+                    if (n.getDest() == destVertex) {
+                        if (n.getE().getWeight() < flow) {
+                            flow = n.getE().getWeight();
+                        }
+                    }
+                }
+                // counter, damit für die Senke / Endknoten nicht mehr nach dem nachbar geschaut wird.
+                counter++;
+                if (counter == pfad.size()-1) {
+                    // counter zurücksetzen auf 0
+                    counter = 0;
+                    break;
+                }
+            }
+
+            for (Vertex v : pfad) {
+                LinkedList<Node> nodes = (LinkedList<Node>) residual.getNeighbors(v);
+                Vertex destVertex = pfad.get((pfad.indexOf(v)) + 1);
+                for (Node n : nodes) {
+                    if (n.getDest() == destVertex) {
+                        n.getE().weight = n.getE().weight - flow;
+                    }
+                }
+
+                //System.out.println(v + " - " + nodes);
+
+                counter++;
+                if (counter == pfad.size()-1) {
+                    break;
+                }
+            }
+
+
+            for (int i = pfad.size()-1; i > 0; i--) {
+
+                Vertex v = pfad.get(i);
+
+                // nachbarn von v
+                LinkedList<Node> nodes = (LinkedList<Node>) residual.getNeighbors(v);
+                Vertex destVertex = pfad.get((pfad.indexOf(v)) - 1);
+
+                for (Node n : nodes) {
+                    if (n.getDest() == destVertex) {
+                        n.getE().weight = n.getE().weight + flow;
+                    }
+                }
 
 
 
-    public void solve(){
 
+            }
+
+            this.maxFlow = this.maxFlow + flow;
+
+        }
+
+        System.out.println(this.maxFlow);
     }
 }
--- GraphSchaft/src/com/hosthans/Algorithms/Wasserleitung/test.java
+++ GraphSchaft/src/com/hosthans/Algorithms/Wasserleitung/test.java
@@ -1,13 +1,39 @@
 package com.hosthans.Algorithms.Wasserleitung;
 
+import com.hosthans.Graph.Edge;
 import com.hosthans.Graph.Graph;
+import com.hosthans.Graph.Graph_NOINPUT;
+import com.hosthans.Graph.Vertex;
 
 import java.io.IOException;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        Graph graph = new Graph(false, true);
-        System.out.println(graph.printGraph());
-        MaxFlow ford = new MaxFlow("a", "c", graph);
+        /*Graph graph = new Graph(false, true);
+        System.out.println(graph.printGraph());*/
+        Graph_NOINPUT graph = new Graph_NOINPUT(false, true);
+        Vertex a = new Vertex("a");
+        Vertex b = new Vertex("b");
+        Vertex c = new Vertex("c");
+        Vertex d = new Vertex("d");
+        Vertex e = new Vertex("e");
+        Vertex f = new Vertex("f");
+        Vertex g = new Vertex("g");
+
+        graph.addEdge(a, b, false, 7);
+        graph.addEdge(a,c,false, 1);
+        graph.addEdge(b,d,false,6);
+        graph.addEdge(b,f,false,8);
+        graph.addEdge(d,f,false,3);
+        graph.addEdge(c,e,false,6);
+        graph.addEdge(d,g,false,2);
+        graph.addEdge(d,e,false,9);
+        graph.addEdge(e,g,false,7);
+        graph.addEdge(f,g,false,1);
+
+
+
+        MaxFlow mf = new MaxFlow(a, g, graph);
     }
+
 }
